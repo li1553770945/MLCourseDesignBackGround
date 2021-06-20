575ce932f38c527dcb164880933da62d63b43819
li1553770945 <1553770945@qq.com>
2021-06-20 17:29:02 +0800

    实现上传音频文件、下载回复文件功能

--- CourseDesign/urls.py
+++ CourseDesign/urls.py
@@ -15,7 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+from reply.views import ReplyView
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('reply/',ReplyView.as_view()),
 ]
